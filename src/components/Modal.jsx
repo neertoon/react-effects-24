f72c565c7d7cba84abc5b0e70f50850bef6a078e
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-01 17:58:57 +0100

    Chyba oszukałem Reacta :D

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,8 +4,14 @@ import { createPortal } from 'react-dom';
 function Modal({ children, open }) {
   const dialog = useRef();
 
+  if (open) {
+    setTimeout(() => dialog.current.showModal(), 200);
+  } else {
+    setTimeout(() => dialog.current.close(), 200);
+  }
+
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById('modal')
