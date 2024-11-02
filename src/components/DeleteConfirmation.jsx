4f33a644ab440502ebcb865d94a80182afce2f73
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-02 17:20:03 +0100

    useEffect - progress bar

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,6 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({onConfirm, onCancel}) {
+    const [remainingTime, setRemainingTime] = useState(TIMER);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('INTERVAL');
+            setRemainingTime(prevRemainingTime=> prevRemainingTime - 10);
+        }, 10);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+
     useEffect(() => {
         console.log('TIMER SET');
         const timer = setTimeout(() => {
@@ -26,6 +42,7 @@ export default function DeleteConfirmation({onConfirm, onCancel}) {
                     Yes
                 </button>
             </div>
+            <progress value={remainingTime} max={TIMER}/>
         </div>
     );
 }
